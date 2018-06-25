f22a7a3d3597581642adeb96e0fc66a2ca2d9e3b
Viktor Svyatokha <ahydrax@gmail.com>
2018-06-25 13:39:20 +1000

    Removed dead code

--- src/Hangfire.PostgreSql/Utils.cs
+++ src/Hangfire.PostgreSql/Utils.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using Hangfire.Common;
 using Hangfire.PostgreSql.Entities;
 using Hangfire.Storage;
@@ -9,34 +8,7 @@ namespace Hangfire.PostgreSql
 {
     internal static class Utils
     {
-        public static bool TryExecute<T>(
-            Func<T> func,
-            out T result,
-            Func<Exception, bool> smoothExValidator = default(Func<Exception, bool>),
-            int? tryCount = default(int?))
-        {
-            while (tryCount == default(int?) || tryCount-- > 0)
-            {
-                try
-                {
-                    result = func();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    if (smoothExValidator != null && !smoothExValidator(ex))
-                    {
-                        throw;
-                    }
-                }
-            }
-
-            result = default(T);
-            return false;
-        }
-
-
-        public delegate TDto JobSelector<TDto>(SqlJob sqlJob, Common.Job job, Dictionary<string, string> state);
+        public delegate TDto JobSelector<TDto>(SqlJob sqlJob, Job job, Dictionary<string, string> state);
         public static JobList<TDto> DeserializeJobs<TDto>(ICollection<SqlJob> jobs, JobSelector<TDto> selector)
         {
             var result = new List<KeyValuePair<string, TDto>>(jobs.Count);
@@ -52,7 +24,7 @@ namespace Hangfire.PostgreSql
             return new JobList<TDto>(result);
         }
 
-        public static Common.Job DeserializeJob(string invocationData, string arguments)
+        public static Job DeserializeJob(string invocationData, string arguments)
         {
             var data = JobHelper.FromJson<InvocationData>(invocationData);
             data.Arguments = arguments;
